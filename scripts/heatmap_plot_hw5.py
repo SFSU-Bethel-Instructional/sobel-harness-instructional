4718bd4a5b767cf46b41d2d5de33a82db3fcd892
Wes Bethel <ewbethel@lbl.gov>
2021-10-13 13:24:24 -0700

    initial entry

---
+++ scripts/heatmap_plot_hw5.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+threads_per_block = ['32', '64', '128', '256', '512', '1024'] # y axis, 6 of them
+thread_blocks = ["1", "4", "16", "64", "256", "1024", "4096"] # x axis, 7 of them
+
+runtime = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
+                    [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
+                    [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
+                    [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
+                    [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
+                    [1.3, 1.2, 0.0, 0.0, 0.0, 3.2, 5.1]])
+
+
+fig, ax = plt.subplots()
+im = ax.imshow(runtime, cmap="coolwarm")
+
+# We want to show all ticks...
+ax.set_xticks(np.arange(len(thread_blocks)))
+ax.set_yticks(np.arange(len(threads_per_block)))
+# ... and label them with the respective list entries
+ax.set_xticklabels(thread_blocks)
+ax.set_yticklabels(threads_per_block)
+
+# Rotate the tick labels and set their alignment.
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+for i in range(len(threads_per_block)): # y axis
+    for j in range(len(thread_blocks)): # x axis
+        text = ax.text(j, i, runtime[i, j],
+                       ha="center", va="center", color="k")
+
+ax.set_title("Runtime on <what platform> at Varying Block Size and Number of Blocks")
+ax.set_ylabel('Threads per block')
+ax.set_xlabel('Block Sizes')
+fig.colorbar(im, ax=ax)
+fig.tight_layout()
+plt.show()
